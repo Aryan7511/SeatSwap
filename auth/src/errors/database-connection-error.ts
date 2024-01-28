d4147dcd26c01d50f5dd4d424452f48bee15b3c5
aryan <comauro7511@gmail.com>
2024-01-29 04:06:08 +0530

    added error handling part

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,8 +1,19 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+
+export class DatabaseConnectionError extends CustomError {
+  statusCode = 500;
   reason = 'error connecting to the database';
   constructor() {
-    super();
+    super('error connecting to the db');
     //Only because we are extending a built-in class
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason
+      }
+    ];
   }
 }
--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,9 +1,31 @@
 import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError  {
+  statusCode = 400;
   constructor(public errors: ValidationError[]) {
-    super();
+    super('Invalid Requests Parameters');
     //Only because we are extending a built-in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
+
+  serializeErrors() {
+    const formattedErrors = this.errors.map((err) => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: 'Validation error', field: 'Unknown' };
+    });
+    return formattedErrors;
+  }
 }
+
+/*export class RequestValidationError extends Error {
+  public errors: ValidationError[];
+
+  constructor(errors: ValidationError[]) {
+    super();
+    this.errors = errors;
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+  }
+} */
