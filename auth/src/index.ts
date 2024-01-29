dd94ec8434c95ac430a0426e0a0a69b33a6793fa
aryan <comauro7511@gmail.com>
2024-01-29 06:28:28 +0530

    database connection and basic user model done

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { json } from 'body-parser';
+import mongoose, { mongo } from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup';
@@ -20,6 +21,17 @@ app.all('*', () => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!!!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to MongoDb');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!');
+  });
+};
+
+start();
---
+++ auth/src/models/user.ts
@@ -0,0 +1,54 @@
+import mongoose, { mongo } from 'mongoose';
+import { Password } from '../services/password';
+
+/*All these interfaces that made is for removing the issues 
+of typescript with mongoose . Actually it's just a work around */
+
+//An interface that describes the properties
+// that are required to create a new user
+interface UserAtrrs {
+  email: string;
+  password: string;
+}
+
+//An interface that describes the properties that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAtrrs): UserDoc;
+}
+
+//An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+//important concept here don't use ()=> arrow function in the argument
+//for more clearance watch the video
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+//important trick to get the work of the typescript to check the attribute type
+//before creating new user
+userSchema.statics.build = (attrs: UserAtrrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
