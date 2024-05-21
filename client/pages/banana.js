1ea271b13b10af67e7b6ca81bf53a9b44d79eaa5
aryan <comauro7511@gmail.com>
2024-05-22 02:47:45 +0530

    signup signin signout working correctly

--- client/pages/banana.js
+++
@@ -1,5 +0,0 @@
-const banana = () => {
-  return <div>banana file</div>;
-};
-
-export default banana;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,34 +1,19 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <div>LandingPage</div>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
 
-/*we created useRequest hook to make it really easy to make some kind of request and possibly handle
-any errors that might occur.
-The reason for this is that useRequest is a hook and hooks are used inside of react components.
-Get initial props right here is not a component. It is a plane function.*/
+LandingPage.getInitialProps = async (context) => {
+  console.log('Landing Page');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    //we are on the server!
-    // requests should be made to http://ingress-nginx-controller_____blah blah
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-
-  return {};
+  return data;
 };
 
 export default LandingPage;
