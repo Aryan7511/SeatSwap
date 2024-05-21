13d16b78f13f4fd40a9e18bab9c93311fd6e1d4c
aryan <comauro7511@gmail.com>
2024-05-22 00:59:30 +0530

    getIntitalProps server rendering api issue resolved

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,34 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the component', color);
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <div>LandingPage</div>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('I am on a server ');
-  return { color: 'red' };
+/*we created useRequest hook to make it really easy to make some kind of request and possibly handle
+any errors that might occur.
+The reason for this is that useRequest is a hook and hooks are used inside of react components.
+Get initial props right here is not a component. It is a plane function.*/
+
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    //we are on the server!
+    // requests should be made to http://ingress-nginx-controller_____blah blah
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+
+  return {};
 };
 
 export default LandingPage;
